a5856deaa2a16e5e967af6fa5634602424b06a6f
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-17 20:04:24 +0300

    Added more questions to the quiz

---
+++ QuizApp/script.js
@@ -0,0 +1,67 @@
+const questions = [
+  {
+    question: "Which is the largest animal in the world?",
+    answer: [
+      { text: "Shark", correct: false },
+      { text: "Blue Whale", correct: true },
+      { text: "Elephant", correct: false },
+      { text: "Giraffe", correct: false },
+    ],
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    answer: [
+      { text: "Ag", correct: false },
+      { text: "Au", correct: true },
+      { text: "Pb", correct: false },
+      { text: "Fe", correct: false },
+    ],
+  },
+  {
+    question: "Which is the largest planet in the solar system?",
+    answer: [
+      { text: "Jupiter", correct: true },
+      { text: "Saturn", correct: false },
+      { text: "Earth", correct: false },
+      { text: "Mars", correct: false },
+    ],
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    answer: [
+      { text: "Vincent Van Gogh", correct: false },
+      { text: "Leonardo da Vinci", correct: true },
+      { text: "Pablo Picasso", correct: false },
+      { text: "Claude Monet", correct: false },
+    ],
+  },
+  {
+    question: "What is the capital city of Australia?",
+    answer: [
+      { text: "Sydney", correct: false },
+      { text: "Melbourne", correct: false },
+      { text: "Canberra", correct: true },
+      { text: "Brisbane", correct: false },
+    ],
+  },
+
+  {
+    question: "Which language is the most spoken worldwide?",
+    answer: [
+      { text: "English", correct: false },
+      { text: "Spanish", correct: false },
+      { text: "Mandarin Chinese", correct: true },
+      { text: "Hindi", correct: false },
+    ],
+  },
+  {
+    question: "Which ocean is the largest?",
+    answer: [
+      { text: "Atlantic Ocean", correct: false },
+      { text: "Indian Ocean", correct: false },
+      { text: "Arctic Ocean", correct: false },
+      { text: "Pacific Ocean", correct: true },
+    ],
+  },
+];
+
