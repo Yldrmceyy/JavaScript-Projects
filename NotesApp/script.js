9cd32a4ab340436d8071a4a38f6ffce1c0ec9e1c
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-18 22:04:18 +0300

    added click event on img elements to delete note

--- NotesApp/script.js
+++ NotesApp/script.js
@@ -3,13 +3,18 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelector(".input-box");
 
+// Add click event to "Create Notes" button
 createBtn.addEventListener("click", () => {
-    let inputBox = document.createElement("p");
-    let img = document.createElement("img");
-    inputBox.className="input-box";
-    inputBox.setAttribute("contenteditable", "true");
-    img.src="./images/delete.png";
-    notesContainer.appendChild(inputBox).appendChild(img);
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "./images/delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
 
+  //add click event to img element
+  img.addEventListener("click", () => {
+    inputBox.remove();
   });
-  
+});
+
