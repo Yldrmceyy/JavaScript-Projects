735ba318a9793125a9ded2ee1d7b1de4e80874d4
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-18 21:35:44 +0300

    Elements in html selected.

---
+++ NotesApp/script.js
@@ -0,0 +1,5 @@
+//Selecting elements
+const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelector(".input-box");
+
