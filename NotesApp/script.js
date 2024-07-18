6c4d1b2b216efbaaeae02c6dc8d0b23c9dbe5a9d
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-18 22:19:24 +0300

    Added localStorage support for notes; handle delete and input changes

--- NotesApp/script.js
+++ NotesApp/script.js
@@ -3,6 +3,17 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelector(".input-box");
 
+//localStorage'dan notları yükler.
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+//localStorage'a notları kaydeder.
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 // Add click event to "Create Notes" button
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
@@ -12,15 +23,24 @@ createBtn.addEventListener("click", () => {
   img.src = "./images/delete.png";
   notesContainer.appendChild(inputBox).appendChild(img);
 
-  //add click event to img element
+  /*add click event to img element
   img.addEventListener("click", () => {
     inputBox.remove();
-  });
+    updateStorage();
+  });*/
 });
 
-/*Added alternative method for deleting notes by handling click events directly on notesContainer
+//Added alternative method for deleting notes by handling click events directly on notesContainer
 notesContainer.addEventListener("click", function(e){
     if(e.target.tagName==="IMG"){
         e.target.parentElement.remove();
+        updateStorage();
+    }else if(e.target.tagName==="P"){
+        notes=document.querySelectorAll(".input-box");
+        notes.forEach(note => {
+            note.onkeyup = function(){
+                updateStorage();
+            }
+        })
     }
-})*/
+})
