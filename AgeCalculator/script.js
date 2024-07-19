0ddb6d838670c1973a59a23d48a4db1248955664
Ceylan Yıldırım <yldrmceylan.1@gmail.com>
2024-07-19 10:31:09 +0300

    Added logic to compare birth date and current date components.

--- AgeCalculator/script.js
+++ AgeCalculator/script.js
@@ -2,11 +2,13 @@ let userInput = document.getElementById("date");
 userInput.max = new Date().toISOString().split("T")[0];
 
 function calculateAge(){
+    //Getting the user's birth date and creates a Date object
     let birthDate = new Date(userInput.value);
     let d1=birthDate.getDate();
     let m1=birthDate.getMonth()+1;
     let y1=birthDate.getFullYear();
 
+    // Getting today's date and creates a Date object
     let today=new Date();
     let d2=today.getDate();
     let m2=today.getMonth()+1;
